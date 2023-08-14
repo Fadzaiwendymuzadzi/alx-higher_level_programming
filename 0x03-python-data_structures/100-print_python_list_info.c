9fbe1ac0566001e7c323f98de103c59a8aa82b1c
Fadzaiwendymuzadzi <fadzaiwendym@gmail.com>
2023-08-14 00:27:09 -0700

    all tasks

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,11 @@
+#include <Python.h>
+#include <stdio.h>
+
+/**
+ * print_python_list_info - gives data of the PyListObject
+ *
+ * @p: the PyObject
+ */
+
+void print_python_list_info(PyObject *p)
+{
